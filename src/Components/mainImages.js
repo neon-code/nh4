1808295cc8f58f6e4425f1e04b021d6866e56d5a
neon-code <siddhm98@yahoo.in>
2018-07-04 14:47:33 +0900

    init commit:

--- src/Components/mainImages.js
+++ src/Components/mainImages.js
@@ -3,56 +3,56 @@ import '../CSS/primary.css';
 import { Button, Image, Icon, Label, Header, Modal } from 'semantic-ui-react';
 
 var fileDir = [
-    { fileName: './50images/n01518878_103.JPEG', id: "n01518878" },
-    { fileName: './50images/n01614925_52.JPEG', id: "n01614925" },
-    { fileName: './50images/n01667114_73.JPEG', id: "n01667114" },
-    { fileName: './50images/n01688243_225.JPEG', id: "n01688243" },
-    { fileName: './50images/n01698640_254.JPEG', id: "n01698640" },
-    { fileName: './50images/n01729322_407.JPEG', id: "n01729322" },
-    { fileName: './50images/n01748264_452.JPEG', id: "n01748264" },
-    { fileName: './50images/n01784675_81.JPEG', id: "n01784675" },
-    { fileName: './50images/n01847000_573.JPEG', id: "n01847000" },
-    { fileName: './50images/n01877812_245.JPEG', id: "n01877812" },
-    { fileName: './50images/n02002724_125.JPEG', id: "n02002724" },
-    { fileName: './50images/n02033041_10.JPEG', id: "n02033041" },
-    { fileName: './50images/n02089078_4441.JPEG', id: "n02089078" },
-    { fileName: './50images/n02093859_3848.JPEG', id: "n02093859" },
-    { fileName: './50images/n02099601_7873.JPEG', id: "n02099601" },
-    { fileName: './50images/n02100877_9560.JPEG', id: "n02100877" },
-    { fileName: './50images/n02110806_15653.JPEG', id: "n02110806" },
-    { fileName: './50images/n02124075_20.JPEG', id: "n02124075" },
-    { fileName: './50images/n02172182_149.JPEG', id: "n02172182" },
-    { fileName: './50images/n02226429_341.JPEG', id: "n02226429" },
-    { fileName: './50images/n02231487_7.JPEG', id: "n02231487" },
-    { fileName: './50images/n02268853_63.JPEG', id: "n02268853" },
-    { fileName: './50images/n02326432_416.JPEG', id: "n02326432" },
-    { fileName: './50images/n02364673_59.JPEG', id: "n02364673" },
-    { fileName: './50images/n02454379_114.JPEG', id: "n02454379" },
-    { fileName: './50images/n02667093_111.JPEG', id: "n02667093" },
-    { fileName: './50images/n02791270_589.JPEG', id: "n02791270" },
-    { fileName: './50images/n02834397_13.JPEG', id: "n02834397" },
-    { fileName: './50images/n02894605_938.JPEG', id: "n02894605" },
-    { fileName: './50images/n02951585_474.JPEG', id: "n02951585" },
-    { fileName: './50images/n02977058_1158.JPEG', id: "n02977058" },
-    { fileName: './50images/n03045698_160.JPEG', id: "n03045698" },
-    { fileName: './50images/n03124170_327.JPEG', id: "n03124170" },
-    { fileName: './50images/n03240683_283.JPEG', id: "n03240683" },
-    { fileName: './50images/n03345487_360.JPEG', id: "n03345487" },
-    { fileName: './50images/n03400231_33.JPEG', id: "n03400231" },
-    { fileName: './50images/n03467068_92.JPEG', id: "n03467068" },
-    { fileName: './50images/n03594734_188.JPEG', id: "n03594734" },
-    { fileName: './50images/n03658185_36.JPEG', id: "n03658185" },
-    { fileName: './50images/n03676483_596.JPEG', id: "n03676483" },
-    { fileName: './50images/n03692522_283.JPEG', id: "n03692522" },
-    { fileName: './50images/n03776460_614.JPEG', id: "n03776460" },
-    { fileName: './50images/n03796401_295.JPEG', id: "n03796401" },
-    { fileName: './50images/n03903868_5674.JPEG', id: "n03903868" },
-    { fileName: './50images/n03977966_691.JPEG', id: "n03977966" },
-    { fileName: './50images/n04037443_252.JPEG', id: "n04037443" },
-    { fileName: './50images/n04147183_395.JPEG', id: "n04147183" },
-    { fileName: './50images/n04270147_24.JPEG', id: "n04270147" },
-    { fileName: './50images/n04442312_168.JPEG', id: "n04442312" },
-    { fileName: './50images/n04525305_430.JPEG', id: "n04525305" },
+    { fileName: './50images/n01532829_26.JPEG', id: "n01532829" },
+    { fileName: './50images/n01622779_70.JPEG', id: "n01622779" },
+    { fileName: './50images/n01667778_35.JPEG', id: "n01667778" },
+    { fileName: './50images/n01693334_143.JPEG', id: "n01693334" },
+    { fileName: './50images/n01697457_118.JPEG', id: "n01697457" },
+    { fileName: './50images/n01734418_109.JPEG', id: "n01734418" },
+    { fileName: './50images/n01756291_309.JPEG', id: "n01756291" },
+    { fileName: './50images/n01818515_244.JPEG', id: "n01818515" },
+    { fileName: './50images/n01871265_89.JPEG', id: "n01871265" },
+    { fileName: './50images/n01873310_379.JPEG', id: "n01873310" },
+    { fileName: './50images/n02007558_149.JPEG', id: "n02007558" },
+    { fileName: './50images/n02051845_11.JPEG', id: "n02051845" },
+    { fileName: './50images/n02090721_1717.JPEG', id: "n02090721" },
+    { fileName: './50images/n02096051_6775.JPEG', id: "n02096051" },
+    { fileName: './50images/n02097298_12041.JPEG', id: "n02097298" },
+    { fileName: './50images/n02101388_13498.JPEG', id: "n02101388" },
+    { fileName: './50images/n02112137_5955.JPEG', id: "n02112137" },
+    { fileName: './50images/n02137549_19.JPEG', id: "n02137549" },
+    { fileName: './50images/n02177972_2.JPEG', id: "n02177972" },
+    { fileName: './50images/n02206856_132.JPEG', id: "n02206856" },
+    { fileName: './50images/n02233338_47.JPEG', id: "n02233338" },
+    { fileName: './50images/n02277742_57.JPEG', id: "n02277742" },
+    { fileName: './50images/n02342885_33.JPEG', id: "n02342885" },
+    { fileName: './50images/n02443114_332.JPEG', id: "n02443114" },
+    { fileName: './50images/n02445715_70.JPEG', id: "n02445715" },
+    { fileName: './50images/n02692877_313.JPEG', id: "n02692877" },
+    { fileName: './50images/n02793495_173.JPEG', id: "n02793495" },
+    { fileName: './50images/n02841315_250.JPEG', id: "n02841315" },
+    { fileName: './50images/n02906734_75.JPEG', id: "n02906734" },
+    { fileName: './50images/n02948072_386.JPEG', id: "n02948072" },
+    { fileName: './50images/n02980441_639.JPEG', id: "n02980441" },
+    { fileName: './50images/n03047690_361.JPEG', id: "n03047690" },
+    { fileName: './50images/n03160309_627.JPEG', id: "n03160309" },
+    { fileName: './50images/n03271574_572.JPEG', id: "n03271574" },
+    { fileName: './50images/n03297495_93.JPEG', id: "n03297495" },
+    { fileName: './50images/n03404251_150.JPEG', id: "n03404251" },
+    { fileName: './50images/n03481172_408.JPEG', id: "n03481172" },
+    { fileName: './50images/n03594945_211.JPEG', id: "n03594945" },
+    { fileName: './50images/n03662601_373.JPEG', id: "n03662601" },
+    { fileName: './50images/n03673027_359.JPEG', id: "n03673027" },
+    { fileName: './50images/n03729826_226.JPEG', id: "n03729826" },
+    { fileName: './50images/n03777568_271.JPEG', id: "n03777568" },
+    { fileName: './50images/n03837869_161.JPEG', id: "n03837869" },
+    { fileName: './50images/n03930313_1116.JPEG', id: "n03930313" },
+    { fileName: './50images/n03954731_9.JPEG', id: "n03954731" },
+    { fileName: './50images/n04044716_101.JPEG', id: "n04044716" },
+    { fileName: './50images/n04154565_753.JPEG', id: "n04154565" },
+    { fileName: './50images/n04285008_296.JPEG', id: "n04285008" },
+    { fileName: './50images/n04465501_75.JPEG', id: "n04465501" },
+    { fileName: './50images/n04505470_250.JPEG', id: "n04505470" },    
 ]
 
 var activeIndex = Math.floor(Math.random() * 50), usedImages = [activeIndex], taskDone = 1, flag;
@@ -66,7 +66,8 @@ export class MainImages extends React.Component {
 
         this.state = {
             isopen: false,
-            activeNext: false
+            activeNext: false,
+            buttonText: "Next Button"
         };
     }
 
@@ -142,6 +143,9 @@ export class MainImages extends React.Component {
         //Load next image
         taskDone++;
         //Change here to lock the images
+        if( taskDone === 50 )
+        this.setState({ buttonText: "Finish!" })
+
         if (taskDone > 50) {
             this.setState({
                 isopen: true
@@ -178,13 +182,13 @@ export class MainImages extends React.Component {
                 <div className="NextButton" style={{ width: '180px' }} >
                     {this.state.activeNext ?
                         <Button primary animated size='huge' onClick={this.changeImage.bind(this)}>
-                            <Button.Content visible> Next Image </Button.Content>
-                            <Button.Content hidden>
-                                <Icon name='right arrow' />
-                            </Button.Content>
-                        </Button>
-                        :
-                        <Button disabled size='huge'>Next Image</Button>
+                        <Button.Content visible> {this.state.buttonText} </Button.Content>
+                        <Button.Content hidden>
+                            <Icon name='right arrow' />
+                        </Button.Content>
+                    </Button>
+                    :
+                    <Button disabled size='huge'>{this.state.buttonText}</Button>
                     }
                 </div>
 
